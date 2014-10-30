5b4ac334472c4e251e95bcc0aa5dd7669563dbd7
Vishnu Sudhakaran <onlinevishnu@gmail.com>
2014-10-30 22:27:29 +0530

    Added comments here n there :)

--- blog.py
+++ blog.py
@@ -1,6 +1,19 @@
+"""
+	A basic blog system built on Flask web framework.
+	Credits for Mysql Wrapper: Kailash Nadh - http://nadh.in - https://github.com/knadh/simplemysql
+	Foundation of the platform: Flaskr http://flask.pocoo.org/docs/0.10/tutorial/introduction/
+	About the Flask framework: http://flask.pocoo.org
+
+	License: GNU GPLv2
+
+	Vishnu Sudhakaran, http://vishnus.name 
+	October 30th, 2014
+
+"""
+
 import sqlite3, time
 from flask import Flask, request, session, redirect, url_for, abort, render_template, flash
-from simplemysql import SimpleMysql
+from simplemysql import SimpleMysql  # mysql wrapper 
 
 db = SimpleMysql(
 	host="127.0.0.1",
@@ -10,22 +23,27 @@ db = SimpleMysql(
 	keep_alive=True
 )
 
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
 
+DEBUG = True # not to be used in production
+USERNAME = 'admin'
+PASSWORD = 'default' 
+"""
+ Since single user, I kept the user details here, you can keep this in separate config file as well
+
+"""
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
 def show_entries():
+	""" Show all the entries - basic home page """
 	cur = db.query("SELECT title, text, date, time FROM entries ORDER BY id DESC")
 	entries = [dict(title=row[0], text=row[1], date=row[2], time=row[3]) for row in cur.fetchall()]
 	return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+	""" Add a new entry """
 	if not session.get('logged_in'):
 		abort(401)
 
@@ -38,6 +56,7 @@ def add_entry():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+	""" Login processing """
 	error = None
 	if request.method == 'POST':
 		if request.form['username'] != app.config['USERNAME']:
@@ -52,6 +71,7 @@ def login():
 
 @app.route('/logout')
 def logout():
+	""" Session logout """
 	session.pop('logged_in', None)
 	flash('You are successfully logged out')
 	return redirect(url_for('show_entries'))
