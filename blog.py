1a04559f8ea3abea788313633341c072d3ab84bb
Vishnu Sudhakaran <onlinevishnu@gmail.com>
2014-10-30 21:37:38 +0530

    First version, First commit

---
+++ blog.py
@@ -0,0 +1,60 @@
+import sqlite3, time
+from flask import Flask, request, session, redirect, url_for, abort, render_template, flash
+from simplemysql import SimpleMysql
+
+db = SimpleMysql(
+	host="127.0.0.1",
+	db="blog",
+	user="root",
+	passwd="",
+	keep_alive=True
+)
+
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def show_entries():
+	cur = db.query("SELECT title, text, date, time FROM entries ORDER BY id DESC")
+	entries = [dict(title=row[0], text=row[1], date=row[2], time=row[3]) for row in cur.fetchall()]
+	return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+
+	current_date = time.strftime("%B %d, %Y")
+	current_time = time.strftime("%I:%M %p")
+	db.insert("entries", {"title": request.form["title"], "text": request.form["text"], "date":current_date, "time": current_time });
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid Username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Password'
+		else:
+			session['logged_in'] = True
+			flash('You are logged in successfully')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You are successfully logged out')
+	return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+	app.run()
